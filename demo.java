de979f60a1b40a9aca0102543cf38e749a72c669
stellamariyadass <stellaabinaya2705@gmail.com>
2025-05-20 12:55:45 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -3,5 +3,7 @@ package demo;
 public class Demo {
 public static void main(String[] arg){
   System.out.println("hi java");
+  System.out.println("Selenium");
+  System.out.println("Cucumber");
 }
 }
